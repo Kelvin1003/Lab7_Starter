f97a9dffc877e4c7c553dd043302e06eceead952
Kelvin1003 <kay003@ucsd.edu>
2021-11-05 18:08:25 -0700

    Tset

--- assets/scripts/Router.js
+++ assets/scripts/Router.js
@@ -38,6 +38,7 @@ export class Router {
      * router instance using the 'this' keyword. Substitute 'home' for the variable
      * page
      */
+    this[page] = pageFunc;
   }
 
   /**
@@ -65,5 +66,25 @@ export class Router {
      *     and URL + hash to history
      *  4. Finally, call the stored function for the given page
      */
+    if (!this[page]) {
+      console.log(`The funtion of the ${page} is not exist`);
+      return;
+    }
+
+    let hash;
+    if (page == 'home') {
+      hash = '';
+    }
+    else {
+      hash = `#${page}`;
+    }
+
+    const stateObj = { 'page': page };
+
+    if (!statePopped && location.hash != hash) {
+      history.pushState(stateObj, page, `${location.origin}${hash}`);
+    }
+
+    this[page]();
   }
 }
